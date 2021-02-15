51effb08f8a54a06e7c094ecbcbe075a6b7db4bf
Aqua <aqua0210@163.com>
2021-02-15 12:06:19 +0800

    don't delete whitespaces in markdown mode.

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -4,9 +4,15 @@
 
 (when (maybe-require-package 'markdown-mode)
   (add-auto-mode 'markdown-mode "\\.md\\.html\\'")
-  (with-eval-after-load 'whitespace-cleanup-mode
-    (add-to-list 'whitespace-cleanup-mode-ignore-modes 'markdown-mode)))
+  (add-auto-mode 'gfm-mode "README\\.md\\'")
 
+  (with-eval-after-load 'markdown-mode
+    (setq markdown-command "multimarkdown"))
+
+  (with-eval-after-load 'auto-save
+    (add-hook 'markdown-mode-hook
+              '(lambda ()
+                 (setq-local auto-save-delete-trailing-whitespace nil)))))
 
 (provide 'init-markdown)
 ;;; init-markdown.el ends here
