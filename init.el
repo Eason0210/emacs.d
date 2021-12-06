c1604d5826727ffb655a45b8347553e922e56ec1
Eason0210 <aqua0210@163.com>
2021-12-07 01:20:56 +0800

    Setup yasnippet

--- init.el
+++ init.el
@@ -311,6 +311,30 @@ This is useful when followed by an immediate kill."
     (setq-local completion-styles '(substring orderless))))
 
 
+;; YASnippet
+(use-package yasnippet
+  :diminish yas-minor-mode
+  :hook ((prog-mode org-mode) . yas-minor-mode)
+  :bind (("C-c y i" . yas-insert-snippet)
+         ("C-c y f" . yas-visit-snippet-file)
+         ("C-c y n" . yas-new-snippet)
+         ("C-c y t" . yas-tryout-snippet)
+         ("C-c y l" . yas-describe-tables)
+         ("C-c y g" . yas-global-mode)
+         ("C-c y m" . yas-minor-mode)
+         ("C-c y r" . yas-reload-all)
+         ("C-c y x" . yas-expand)
+         :map yas-keymap
+         ("C-i" . yas-next-field-or-maybe-expand))
+  :config
+  (yas-reload-all))
+
+(use-package yasnippet-snippets
+  :defer t
+  :after yasnippet)
+
+
+
 (use-package dash
   :config (global-dash-fontify-mode 1))
 
