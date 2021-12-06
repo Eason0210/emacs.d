4fb9179f3aedc9928f95dfc884293ed74355534d
Eason0210 <aqua0210@163.com>
2021-12-07 01:02:06 +0800

    Config Ibuffer and uniquification of buffer names

--- init.el
+++ init.el
@@ -228,6 +228,54 @@ This is useful when followed by an immediate kill."
     (goto-char isearch-other-end)))
 
 
+;;; Configure uniquification of buffer names
+
+;; Nicer naming of buffers for files with identical names
+(use-package uniquify
+  :config
+  (setq uniquify-buffer-name-style 'reverse)
+  (setq uniquify-separator " • ")
+  (setq uniquify-after-kill-buffer-p t)
+  (setq uniquify-ignore-buffers-re "^\\*"))
+
+
+;;; Ibuffer settings
+
+(use-package ibuffer
+  :bind ("C-x C-b" . ibuffer)
+  :config
+  (setq-default ibuffer-show-empty-filter-groups nil)
+
+  ;; Use human readable Size column instead of original one
+  (define-ibuffer-column size-h
+    (:name "Size" :inline t)
+    (file-size-human-readable (buffer-size)))
+
+  ;; Modify the default ibuffer-formats (toggle with `)
+  (setq ibuffer-formats
+        '((mark modified read-only vc-status-mini " "
+                (name 22 22 :left :elide)
+                " "
+                (size-h 9 -1 :right)
+                " "
+                (mode 12 12 :left :elide)
+                " "
+                vc-relative-file)
+          (mark modified read-only vc-status-mini " "
+                (name 22 22 :left :elide)
+                " "
+                (size-h 9 -1 :right)
+                " "
+                (mode 14 14 :left :elide)
+                " "
+                (vc-status 12 12 :left)
+                " "
+                vc-relative-file)))
+
+  (setq ibuffer-filter-group-name-face 'font-lock-doc-face))
+
+
+
 (use-package dash
   :config (global-dash-fontify-mode 1))
 
