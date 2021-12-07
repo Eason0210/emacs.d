9c7c2792ecefa03cf35cec805551bcb7224041ec
Eason0210 <aqua0210@163.com>
2021-12-07 19:35:51 +0800

    Add paredit

--- init.el
+++ init.el
@@ -1198,6 +1198,34 @@ typical word processor."
         (writeroom-mode 0)))))
 
 
+;;; Configure paredit structured editing
+
+(use-package paredit
+  :diminish paredit-mode " Par"
+  :hook ((lisp-mode emacs-lisp-mode) . paredit-mode)
+  :bind (:map paredit-mode-map
+              ("[")
+              ;; ("M-k"   . paredit-raise-sexp)
+              ("M-I"   . paredit-splice-sexp)
+              ;; ("C-M-l" . paredit-recentre-on-sexp)
+              ("C-c ( n"   . paredit-add-to-next-list)
+              ("C-c ( p"   . paredit-add-to-previous-list)
+              ("C-c ( j"   . paredit-join-with-next-list)
+              ("C-c ( J"   . paredit-join-with-previous-list))
+  :bind (:map lisp-mode-map       ("<return>" . paredit-newline))
+  :bind (:map emacs-lisp-mode-map ("<return>" . paredit-newline))
+  :hook (paredit-mode
+         . (lambda ()
+             (unbind-key [M-up] paredit-mode-map)
+             (unbind-key [M-down] paredit-mode-map)
+             (unbind-key "M-r" paredit-mode-map)
+             (unbind-key "M-s" paredit-mode-map)))
+  :config
+  (require 'eldoc)
+  (eldoc-add-command 'paredit-backward-delete
+                     'paredit-close-round))
+
+
 ;;; Spell check settings
 
 (use-package flyspell
