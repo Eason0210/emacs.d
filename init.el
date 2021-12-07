e87a01001838e3bcf1b9c15b1079b5cb0e3d4199
Eason0210 <aqua0210@163.com>
2021-12-07 22:39:11 +0800

    Add Emacs config

--- init.el
+++ init.el
@@ -310,6 +310,22 @@ This is useful when followed by an immediate kill."
     (setq-local completion-styles '(substring orderless))))
 
 
+;; A few more useful configurations...
+(use-package emacs
+  :init
+  ;; Do not allow the cursor in the minibuffer prompt
+  (setq minibuffer-prompt-properties
+        '(read-only t cursor-intangible t face minibuffer-prompt))
+  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+
+  ;; Emacs 28: Hide commands in M-x which do not work in the current mode.
+  ;; Vertico commands are hidden in normal buffers.
+  (setq read-extended-command-predicate
+        #'command-completion-default-include-p)
+
+  (setq enable-recursive-minibuffers t))
+
+
 ;; YASnippet
 (use-package yasnippet
   :diminish yas-minor-mode
