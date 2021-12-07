b558735b62517e12dd37268ef3039b47a71b095b
Eason0210 <aqua0210@163.com>
2021-12-07 22:14:24 +0800

    Add quickrun and setup M-x compile

--- init.el
+++ init.el
@@ -916,6 +916,26 @@ Call a second time to restore the original window configuration."
   :after ibuffer projectile)
 
 
+;;; Helpers for M-x compile
+
+(use-package compile
+  :bind ([f6] . recompile)
+  :config
+  (setq-default compilation-scroll-output t))
+
+;;Configuration for quickrun
+(use-package quickrun
+  :bind (("<f5>" . quickrun)
+         ("C-<f5>" . quickrun-shell))
+  :config
+  (quickrun-add-command "c++/c1z"
+                        '((:command . "g++")
+                          (:exec    . ("%c -std=c++1z %o -o %e %s"
+                                       "%e %a"))
+                          (:remove  . ("%e")))
+                        :default "c++"))
+
+
 ;;; Org-mode config
 
 ;; Among settings for many aspects of `org-mode', this code includes
