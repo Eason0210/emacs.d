0caa8f2d8729d65459f58408100a21556decef02
Eason0210 <aqua0210@163.com>
2021-12-08 00:41:14 +0800

    Add LSP client: eglot

--- init.el
+++ init.el
@@ -1500,6 +1500,24 @@ typical word processor."
   :mode "\\.rs\\'")
 
 
+;;; Languages Server Protocol(LSP)
+
+(use-package eglot
+  :defer t
+  :bind (:map eglot-mode-map
+              ("C-c l a" . eglot-code-actions)
+              ("C-c l r" . eglot-rename)
+              ("C-c l f" . eglot-format)
+              ("C-c l d" . eldoc))
+  :hook (eglot-managed-mode . (lambda () (flymake-mode -1)))
+  :config
+  (add-to-list 'eglot-server-programs '(rust-mode "rust-analyzer"))
+  (add-to-list 'eglot-server-programs '((c++-mode c-mode) "clangd"))
+
+  (setq read-process-output-max (* 1024 1024))
+  (push :documentHighlightProvider eglot-ignored-server-capabilities)
+  (setq eldoc-echo-area-use-multiline-p nil))
+
 
 ;;; Configure paredit structured editing
 
