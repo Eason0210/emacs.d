45591a63d85eaa1b2ef244f3069f965d682ee118
Eason0210 <aqua0210@163.com>
2021-12-07 20:08:02 +0800

    Setup Haskell mode

--- init.el
+++ init.el
@@ -1213,6 +1213,34 @@ typical word processor."
 (use-package cmake-mode
   :defer t)
 
+;;; Haskell mode
+
+(use-package haskell-mode
+  :bind (:map haskell-mode-map
+              ("C-c C-f" . ormolu-buffer))
+  :hook ((haskell-mode . interactive-haskell-mode)
+         (haskell-mode . haskell-indentation-mode)
+         (haskell-mode . haskell-auto-insert-module-template)))
+
+(use-package dante
+  :after (haskell-mode flycheck)
+  :hook (haskell-mode . dante-mode)
+  :config
+  (flycheck-add-next-checker 'haskell-dante '(warning . haskell-hlint)))
+
+(use-package reformatter
+  :after haskell-mode
+  :config
+  (reformatter-define hindent
+                      :program "hindent"
+                      :lighter " Hin")
+
+  (defalias 'hindent-mode 'hindent-on-save-mode)
+
+  (reformatter-define ormolu
+                      :program "ormolu"
+                      :lighter " Orm"))
+
 
 ;;; Configure paredit structured editing
 
