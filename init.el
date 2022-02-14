841949179dc9b2d8d25596b0047c46f87d3df9e5
Eason0210 <aqua0210@163.com>
2022-02-14 19:31:46 +0800

    Hack eglot support in flycheck

--- init.el
+++ init.el
@@ -280,8 +280,21 @@ This is useful when followed by an immediate kill."
 (use-package flycheck
   :hook (after-init . global-flycheck-mode)
   :config
+  (setq flycheck-emacs-lisp-load-path 'inherit)
+
+  ;; And don't recheck on idle as often
+  (setq flycheck-idle-change-delay 1.0)
+
+  ;; For the above functionality, check syntax in a buffer that you switched to
+  ;; only briefly. This allows "refreshing" the syntax check state for several
+  ;; buffers quickly after e.g. changing a config file.
+  (setq flycheck-buffer-switch-check-intermediate-buffers t)
+
+  ;; Display errors a little quicker (default is 0.9s)
+  (setq flycheck-display-errors-delay 0.25)
+
   (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list
-        flycheck-check-syntax-automatically '(idle-change new-line mode-enabled)))
+        flycheck-check-syntax-automatically '(idle-change mode-enabled)))
 
 (use-package flycheck-color-mode-line
   :hook (flycheck-mode . flycheck-color-mode-line-mode)
@@ -1587,11 +1600,81 @@ typical word processor."
               ("C-c l r" . eglot-rename)
               ("C-c l f" . eglot-format)
               ("C-c l d" . eldoc))
-  :hook (eglot-managed-mode . (lambda () (flycheck-mode -1)))
   :config
   (setq read-process-output-max (* 1024 1024))
   (push :documentHighlightProvider eglot-ignored-server-capabilities)
-  (setq eldoc-echo-area-use-multiline-p nil))
+  (setq eldoc-echo-area-use-multiline-p nil)
+
+  ;; Hacky eglot support in flycheck
+  ;; This file sets up flycheck so that, when eglot receives a publishDiagnostics method
+  ;; from the server, flycheck updates the reports.
+  ;;
+  ;; It works by creating a bridge function which can be used as the argument of
+  ;; `eglot-flymake-backend', which both consumes diagnostics and queue a call to
+  ;; 'flycheck-buffer'
+
+  (defvar-local +lsp--flycheck-eglot--current-errors nil)
+
+  (defun +lsp--flycheck-eglot-init (checker callback)
+    "CHECKER is the checker (eglot).
+CALLBACK is the function that we need to call when we are done, on all the errors."
+    (eglot-flymake-backend #'+lsp--flycheck-eglot--on-diagnostics)
+    (funcall callback 'finished +lsp--flycheck-eglot--current-errors))
+
+  (defun +lsp--flycheck-eglot--on-diagnostics (diags &rest _)
+    (cl-labels
+        ((flymake-diag->flycheck-err
+           (diag)
+           (with-current-buffer (flymake--diag-buffer diag)
+             (flycheck-error-new-at-pos
+              (flymake--diag-beg diag)
+              (pcase (flymake--diag-type diag)
+                ('eglot-note 'info)
+                ('eglot-warning 'warning)
+                ('eglot-error 'error)
+                (_ (error "Unknown diagnostic type, %S" diag)))
+              (flymake--diag-text diag)
+              :end-pos (flymake--diag-end diag)
+              :checker 'eglot
+              :buffer (current-buffer)
+              :filename (buffer-file-name)))))
+      (setq +lsp--flycheck-eglot--current-errors
+            (mapcar #'flymake-diag->flycheck-err diags))
+      ;; Call Flycheck to update the diagnostics annotations
+      (flycheck-buffer-deferred)))
+
+  (defun +lsp--flycheck-eglot-available-p ()
+    (bound-and-true-p eglot--managed-mode))
+
+  (flycheck-define-generic-checker 'eglot
+    "Report `eglot' diagnostics using `flycheck'."
+    :start #'+lsp--flycheck-eglot-init
+    :predicate #'+lsp--flycheck-eglot-available-p
+    :modes '(prog-mode text-mode))
+
+  (push 'eglot flycheck-checkers)
+
+  (defun +lsp-eglot-prefer-flycheck-h ()
+    (when eglot--managed-mode
+      (flymake-mode -1)
+      (when-let ((current-checker (flycheck-get-checker-for-buffer)))
+        (when (memq current-checker (list 'c/c++-clang 'rust-cargo 'python-pycompile))
+          (flycheck-disable-checker current-checker))
+        (unless (equal current-checker 'eglot)
+          (flycheck-add-next-checker 'eglot current-checker)))
+      (flycheck-add-mode 'eglot major-mode)
+      (flycheck-mode 1)
+      ;; Call flycheck on initilization to make sure to display initial
+      ;; errors
+      (flycheck-buffer-deferred)))
+
+  (add-hook 'eglot-managed-mode-hook #'+lsp-eglot-prefer-flycheck-h)
+
+  (with-eval-after-load 'flymake
+    (when (and
+           (not (fboundp 'flymake--diag-buffer))
+           (fboundp 'flymake--diag-locus))
+      (defalias 'flymake--diag-buffer 'flymake--diag-locus))))
 
 
 ;;; Configure paredit structured editing
