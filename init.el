506c02e312fe102ffe084ec115f82fc6fa324dd0
Eason0210 <aqua0210@163.com>
2022-04-16 20:49:31 +0800

    Tweaks to window config

--- init.el
+++ init.el
@@ -550,6 +550,68 @@ targets."
 
 
 ;;; Working with Windows within frames
+(use-package window
+  :bind (("C-x |" . split-window-horizontally-instead)
+         ("C-x _" . split-window-vertically-instead)
+         ([f7] . sanityinc/split-window)
+         ("C-c <down>". sanityinc/toggle-current-window-dedication))
+  :config
+  (bind-key "C-x 2" (split-window-func-with-other-buffer 'split-window-vertically))
+  (bind-key "C-x 3" (split-window-func-with-other-buffer 'split-window-horizontally))
+  :preface
+  ;; When splitting window, show (other-buffer) in the new window
+  (defun split-window-func-with-other-buffer (split-function)
+    "Use SPLIT-FUNCTION to split window."
+    (lambda (&optional arg)
+      "Split this window and switch to the new window unless ARG is provided."
+      (interactive "P")
+      (funcall split-function)
+      (let ((target-window (next-window)))
+        (set-window-buffer target-window (other-buffer))
+        (unless arg
+          (select-window target-window)))))
+
+  ;; Rearrange split windows
+  (defun split-window-horizontally-instead ()
+    "Kill any other windows and re-split such that the current window is on the top half of the frame."
+    (interactive)
+    (let ((other-buffer (and (next-window) (window-buffer (next-window)))))
+      (delete-other-windows)
+      (split-window-horizontally)
+      (when other-buffer
+        (set-window-buffer (next-window) other-buffer))))
+
+  (defun split-window-vertically-instead ()
+    "Kill any other windows and re-split such that the current window is on the left half of the frame."
+    (interactive)
+    (let ((other-buffer (and (next-window) (window-buffer (next-window)))))
+      (delete-other-windows)
+      (split-window-vertically)
+      (when other-buffer
+        (set-window-buffer (next-window) other-buffer))))
+
+  ;; Borrowed from http://postmomentum.ch/blog/201304/blog-on-emacs
+  (defun sanityinc/split-window()
+    "Split the window to see the most recent buffer in the other window.
+Call a second time to restore the original window configuration."
+    (interactive)
+    (if (eq last-command 'sanityinc/split-window)
+        (progn
+          (jump-to-register :sanityinc/split-window)
+          (setq this-command 'sanityinc/unsplit-window))
+      (window-configuration-to-register :sanityinc/split-window)
+      (switch-to-buffer-other-window nil)))
+
+  ;; Toggle to dedicated window
+  (defun sanityinc/toggle-current-window-dedication ()
+    "Toggle whether the current window is dedicated to its current buffer."
+    (interactive)
+    (let* ((window (selected-window))
+           (was-dedicated (window-dedicated-p window)))
+      (set-window-dedicated-p window (not was-dedicated))
+      (message "Window %sdedicated to %s"
+               (if was-dedicated "no longer " "")
+               (buffer-name)))))
 
 ;; Navigate window layouts with "M-N" and "M-P"
 (use-package winner
@@ -565,70 +627,6 @@ targets."
   :config
   (setq-default switch-window-shortcut-style 'alphabet)
   (setq-default switch-window-timeout nil))
-
-;; When splitting window, show (other-buffer) in the new window
-(defun split-window-func-with-other-buffer (split-function)
-  "Use SPLIT-FUNCTION to split window."
-  (lambda (&optional arg)
-    "Split this window and switch to the new window unless ARG is provided."
-    (interactive "P")
-    (funcall split-function)
-    (let ((target-window (next-window)))
-      (set-window-buffer target-window (other-buffer))
-      (unless arg
-        (select-window target-window)))))
-
-(bind-key "C-x 2" (split-window-func-with-other-buffer 'split-window-vertically))
-(bind-key "C-x 3" (split-window-func-with-other-buffer 'split-window-horizontally))
-
-;; Rearrange split windows
-(defun split-window-horizontally-instead ()
-  "Kill any other windows and re-split such that the current window is on the top half of the frame."
-  (interactive)
-  (let ((other-buffer (and (next-window) (window-buffer (next-window)))))
-    (delete-other-windows)
-    (split-window-horizontally)
-    (when other-buffer
-      (set-window-buffer (next-window) other-buffer))))
-
-(defun split-window-vertically-instead ()
-  "Kill any other windows and re-split such that the current window is on the left half of the frame."
-  (interactive)
-  (let ((other-buffer (and (next-window) (window-buffer (next-window)))))
-    (delete-other-windows)
-    (split-window-vertically)
-    (when other-buffer
-      (set-window-buffer (next-window) other-buffer))))
-
-(bind-key "C-x |" 'split-window-horizontally-instead)
-(bind-key "C-x _" 'split-window-vertically-instead)
-
-;; Borrowed from http://postmomentum.ch/blog/201304/blog-on-emacs
-(defun sanityinc/split-window()
-  "Split the window to see the most recent buffer in the other window.
-Call a second time to restore the original window configuration."
-  (interactive)
-  (if (eq last-command 'sanityinc/split-window)
-      (progn
-        (jump-to-register :sanityinc/split-window)
-        (setq this-command 'sanityinc/unsplit-window))
-    (window-configuration-to-register :sanityinc/split-window)
-    (switch-to-buffer-other-window nil)))
-
-(bind-key "<f7>" 'sanityinc/split-window)
-
-;; Toggle to dedicated window
-(defun sanityinc/toggle-current-window-dedication ()
-  "Toggle whether the current window is dedicated to its current buffer."
-  (interactive)
-  (let* ((window (selected-window))
-         (was-dedicated (window-dedicated-p window)))
-    (set-window-dedicated-p window (not was-dedicated))
-    (message "Window %sdedicated to %s"
-             (if was-dedicated "no longer " "")
-             (buffer-name))))
-
-(bind-key "C-c <down>" 'sanityinc/toggle-current-window-dedication)
 
 
 ;;; Settings for tracking recent files
